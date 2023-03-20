2a448608c43837585cfc684861ba4fa61fa3288d
dapoadedire <adedireadedapo19@gmail.com>
2023-03-20 23:49:34 +0100

    fix: found a better solution

--- typescript/src/sumOfParts.ts
+++ typescript/src/sumOfParts.ts
@@ -1,14 +1,28 @@
-const sumOfArray = (arr: number[]): number => arr.reduce((a, b) => a + b, 0);
+// const sumOfArray = (arr: number[]): number => arr.reduce((a, b) => a + b, 0);
 
 
+
+// export function partsSums(ls: number[]): number[] {
+//     const sums = [];
+//     for (let i = 0; i < ls.length + 1; i++) {
+//         sums.push(sumOfArray(ls.slice(i)));
+//     }
+//     return sums;
+// }
+
+
+// better solution.
 
 export function partsSums(ls: number[]): number[] {
-    const sums = [];
-    for (let i = 0; i < ls.length + 1; i++) {
-        sums.push(sumOfArray(ls.slice(i)));
+    let sum = ls.reduce((a, b) => a + b, 0);
+    const runningSums = [sum];
+    for (let i = 0; i < ls.length; i++) {
+        sum -= ls[i];
+        runningSums.push(sum);
     }
-    return sums;
+    return runningSums;
 }
+
 
 console.log(partsSums([0, 1, 3, 6, 10]));
 console.log(partsSums([1, 2, 3, 4, 5, 6]));
